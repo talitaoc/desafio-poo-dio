b64596341f444bd75e45a49357a5db084dab7322
talitaoc <talitaoc@gmail.com>
2022-08-08 22:18:58 -0300

    second commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descricao curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso1.setTitulo("curso js");
+        curso1.setDescricao("descricao curso js");
+        curso1.setCargaHoraria(4);
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de java");
+        mentoria.setDescricao("descricao mentoria de java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(mentoria);
+
+
+    }
+}
